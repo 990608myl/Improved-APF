1a4104572534abc881f4af4bfcec75d5a18f5160
990608myl <128768222+990608myl@users.noreply.github.com>
2025-03-12 22:24:28 +0800

    Update Dynamic obstacle velocity repulsive algorithm.m

--- Dynamic obstacle velocity repulsive algorithm.m
+++ Dynamic obstacle velocity repulsive algorithm.m
@@ -1,10 +1,9 @@
-%障碍物速度势场
 %         obstacle_force_v = zeros(2,1);
 
         for j = 1:size(obstacle, 1)
             obstacle_pos_v = obstacle(j, :);
             distance_rep_v = norm(robot_pos(i,:)' - obstacle_pos_v');
-%             if distance_rep <= range  % 判断是否在一定范围内
+%             if distance_rep <= range  
                 direction_rep_v = (robot_pos(i, :)' - obstacle_pos') / distance_rep_v;
                 
                 v_diff = norm(obstacle_speed(j, :) - velocity');
